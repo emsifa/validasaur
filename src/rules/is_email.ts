5e8a9b3adfb79765082996212f53641e1875905e
Muhammad Syifa <emsifa@gmail.com>
2020-05-30 07:04:20 +0700

    add isEmail rule

---
+++ src/rules/is_email.ts
@@ -0,0 +1,10 @@
+import { invalid } from "../utils.ts";
+import { RuleReturn } from "../types.ts";
+
+export function isEmail(value: any): RuleReturn {
+  // https://stackoverflow.com/a/46181
+  const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (typeof value !== "string" || !regex.test(value.toLowerCase())) {
+    return invalid("isEmail", { value });
+  }
+}
