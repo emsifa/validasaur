6d346640c8311fcd3e87253f19ea73049e68f00c
Muhammad Syifa <emsifa@gmail.com>
2020-08-29 09:05:20 +0700

    fix typo

--- tests/rules/date_after.test.ts
+++ tests/rules/date_after.test.ts
@@ -1,4 +1,4 @@
-import { dateAfter } from "../../src/rules/date_After.ts";
+import { dateAfter } from "../../src/rules/date_after.ts";
 import { invalid } from "../../src/utils.ts";
 import { assertInvalid, assertValid, fakeUtils } from "../utils.ts";
 import { Validity } from "../../src/types.ts";
@@ -7,23 +7,23 @@ Deno.test("rules.dateAfter() type check", () => {
   const date = new Date();
   assertInvalid(
     dateAfter(date)(5, fakeUtils) as Validity,
-    invalid("dateAfter:typeCheck", { value: 5, date }),
+    invalid("dateAfter:typeCheck", { value: 5, date })
   );
   assertInvalid(
     dateAfter(date)(null, fakeUtils) as Validity,
-    invalid("dateAfter:typeCheck", { value: null, date }),
+    invalid("dateAfter:typeCheck", { value: null, date })
   );
   assertInvalid(
     dateAfter(date)(undefined, fakeUtils) as Validity,
-    invalid("dateAfter:typeCheck", { value: undefined, date }),
+    invalid("dateAfter:typeCheck", { value: undefined, date })
   );
   assertInvalid(
     dateAfter(date)([], fakeUtils) as Validity,
-    invalid("dateAfter:typeCheck", { value: [], date }),
+    invalid("dateAfter:typeCheck", { value: [], date })
   );
   assertInvalid(
     dateAfter(date)({}, fakeUtils) as Validity,
-    invalid("dateAfter:typeCheck", { value: {}, date }),
+    invalid("dateAfter:typeCheck", { value: {}, date })
   );
 });
 
@@ -31,7 +31,7 @@ Deno.test("rules.dateAfter() length check", () => {
   const date = new Date();
   assertInvalid(
     dateAfter(date)("20201002", fakeUtils) as Validity,
-    invalid("dateAfter:lengthCheck", { value: "20201002", date }),
+    invalid("dateAfter:lengthCheck", { value: "20201002", date })
   );
 });
 
@@ -41,21 +41,21 @@ Deno.test("rules.dateAfter() date check", () => {
   // same date
   assertInvalid(
     dateAfter(date)("2020-01-02", fakeUtils) as Validity,
-    invalid("dateAfter", { value: "2020-01-02", date }),
+    invalid("dateAfter", { value: "2020-01-02", date })
   );
   assertInvalid(
     dateAfter(date)(new Date("2020-01-02"), fakeUtils) as Validity,
-    invalid("dateAfter", { value: new Date("2020-01-02"), date }),
+    invalid("dateAfter", { value: new Date("2020-01-02"), date })
   );
 
   // date before
   assertInvalid(
     dateAfter(date)("2020-01-01", fakeUtils) as Validity,
-    invalid("dateAfter", { value: "2020-01-01", date }),
+    invalid("dateAfter", { value: "2020-01-01", date })
   );
   assertInvalid(
     dateAfter(date)(new Date("2020-01-01"), fakeUtils) as Validity,
-    invalid("dateAfter", { value: new Date("2020-01-01"), date }),
+    invalid("dateAfter", { value: new Date("2020-01-01"), date })
   );
 
   // date After
