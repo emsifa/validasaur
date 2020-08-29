a334f8063e11c2e82e7dbd6841ff6298c1c2c7ce
Muhammad Syifa <emsifa@gmail.com>
2020-08-29 13:12:53 +0700

    deno fmt

--- tests/rules/date_between.test.ts
+++ tests/rules/date_between.test.ts
@@ -8,23 +8,23 @@ Deno.test("rules.dateBetween() type check", () => {
   const maxDate = new Date("2020-01-09");
   assertInvalid(
     dateBetween(minDate, maxDate)(5, fakeUtils) as Validity,
-    invalid("dateBetween:typeCheck", { value: 5, minDate, maxDate })
+    invalid("dateBetween:typeCheck", { value: 5, minDate, maxDate }),
   );
   assertInvalid(
     dateBetween(minDate, maxDate)(null, fakeUtils) as Validity,
-    invalid("dateBetween:typeCheck", { value: null, minDate, maxDate })
+    invalid("dateBetween:typeCheck", { value: null, minDate, maxDate }),
   );
   assertInvalid(
     dateBetween(minDate, maxDate)(undefined, fakeUtils) as Validity,
-    invalid("dateBetween:typeCheck", { value: undefined, minDate, maxDate })
+    invalid("dateBetween:typeCheck", { value: undefined, minDate, maxDate }),
   );
   assertInvalid(
     dateBetween(minDate, maxDate)([], fakeUtils) as Validity,
-    invalid("dateBetween:typeCheck", { value: [], minDate, maxDate })
+    invalid("dateBetween:typeCheck", { value: [], minDate, maxDate }),
   );
   assertInvalid(
     dateBetween(minDate, maxDate)({}, fakeUtils) as Validity,
-    invalid("dateBetween:typeCheck", { value: {}, minDate, maxDate })
+    invalid("dateBetween:typeCheck", { value: {}, minDate, maxDate }),
   );
 });
 
@@ -33,7 +33,7 @@ Deno.test("rules.dateBetween() length check", () => {
   const maxDate = new Date("2020-01-09");
   assertInvalid(
     dateBetween(minDate, maxDate)("20201002", fakeUtils) as Validity,
-    invalid("dateBetween:lengthCheck", { value: "20201002", minDate, maxDate })
+    invalid("dateBetween:lengthCheck", { value: "20201002", minDate, maxDate }),
   );
 });
 
@@ -43,50 +43,59 @@ Deno.test("rules.dateBetween() date check", () => {
 
   // date min
   assertValid(
-    dateBetween(minDate, maxDate)("2020-01-02", fakeUtils) as Validity
+    dateBetween(minDate, maxDate)("2020-01-02", fakeUtils) as Validity,
   );
   assertValid(
-    dateBetween(minDate, maxDate)(new Date("2020-01-02"), fakeUtils) as Validity
+    dateBetween(minDate, maxDate)(
+      new Date("2020-01-02"),
+      fakeUtils,
+    ) as Validity,
   );
   // date max
   assertValid(
-    dateBetween(minDate, maxDate)("2020-01-09", fakeUtils) as Validity
+    dateBetween(minDate, maxDate)("2020-01-09", fakeUtils) as Validity,
   );
   assertValid(
-    dateBetween(minDate, maxDate)(new Date("2020-01-09"), fakeUtils) as Validity
+    dateBetween(minDate, maxDate)(
+      new Date("2020-01-09"),
+      fakeUtils,
+    ) as Validity,
   );
 
   // below date min
   assertInvalid(
     dateBetween(minDate, maxDate)("2020-01-01", fakeUtils) as Validity,
-    invalid("dateBetween", { value: "2020-01-01", minDate, maxDate })
+    invalid("dateBetween", { value: "2020-01-01", minDate, maxDate }),
   );
   assertInvalid(
     dateBetween(minDate, maxDate)(
       new Date("2020-01-01"),
-      fakeUtils
+      fakeUtils,
     ) as Validity,
-    invalid("dateBetween", { value: new Date("2020-01-01"), minDate, maxDate })
+    invalid("dateBetween", { value: new Date("2020-01-01"), minDate, maxDate }),
   );
 
   // above date max
   assertInvalid(
     dateBetween(minDate, maxDate)("2020-01-10", fakeUtils) as Validity,
-    invalid("dateBetween", { value: "2020-01-10", minDate, maxDate })
+    invalid("dateBetween", { value: "2020-01-10", minDate, maxDate }),
   );
   assertInvalid(
     dateBetween(minDate, maxDate)(
       new Date("2020-01-10"),
-      fakeUtils
+      fakeUtils,
     ) as Validity,
-    invalid("dateBetween", { value: new Date("2020-01-10"), minDate, maxDate })
+    invalid("dateBetween", { value: new Date("2020-01-10"), minDate, maxDate }),
   );
 
   // date between
   assertValid(
-    dateBetween(minDate, maxDate)("2020-01-07", fakeUtils) as Validity
+    dateBetween(minDate, maxDate)("2020-01-07", fakeUtils) as Validity,
   );
   assertValid(
-    dateBetween(minDate, maxDate)(new Date("2020-01-07"), fakeUtils) as Validity
+    dateBetween(minDate, maxDate)(
+      new Date("2020-01-07"),
+      fakeUtils,
+    ) as Validity,
   );
 });
